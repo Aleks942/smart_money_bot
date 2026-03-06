4a96fcb059982242316580621467b77e05bf3daa
Aleks942 <aleks.aw1978@gmail.com>
2026-03-06 11:53:55 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1608,9 +1608,10 @@ if __name__ == "__main__":
                         trigger_mark(state, instId, "last_confirm_trigger_ts")
 
 
-                    update_symbol_state(state, sig)
+                                       update_symbol_state(state, sig)
                     time.sleep(0.14)
-                except:
+                except Exception as e:
+                    print(f"[SCAN ERROR] {instId}: {e}", flush=True)
                     continue
 
                     # =====================
