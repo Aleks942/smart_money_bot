3678ee5b1b578ff1fee64acd3e4390ef0a5dc0f6
Aleks942 <aleks.aw1978@gmail.com>
2026-03-08 04:30:32 +0100

    Update main.py

--- main.py
+++ main.py
@@ -2036,7 +2036,7 @@ if __name__ == "__main__":
 
 
                     update_symbol_state(state, sig)
-                    time.sleep(0.14)
+                    time.sleep(0.12)
 
                 except Exception as e:
                     print(f"[SCAN ERROR] {instId}: {e}", flush=True)
