c6db80f405d627920de6ccfbd080baaf5ad4f536
Aleks942 <aleks.aw1978@gmail.com>
2026-03-17 22:39:35 +0100

    Update main.py

--- main.py
+++ main.py
@@ -391,7 +391,7 @@ def send_telegram(text: str):
         )
 
     except Exception as e:
-    print(f"TELEGRAM ERROR: {e}")
+        print(f"TELEGRAM ERROR: {e}")
 
 # =========================
 # STATE
