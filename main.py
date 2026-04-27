3ab62b11037ef12e81feaeb6f7a515fcdcd139ac
Aleks942 <aleks.aw1978@gmail.com>
2026-04-27 10:14:32 +0200

    Update main.py

--- main.py
+++ main.py
@@ -4878,7 +4878,7 @@ if "высокий" in risk_label.lower():
     elif title == "🟢 GOOD SETUP":
         title = "🟡 WATCH"
 
-return total, title
+    return total, title
 
 except:
     return 0, "🔴 SKIP"
