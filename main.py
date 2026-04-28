0e900054728a486af9b49d0e6e63afb1f7c4d91e
Aleks942 <aleks.aw1978@gmail.com>
2026-04-28 20:50:33 +0200

    Update main.py

--- main.py
+++ main.py
@@ -6657,7 +6657,7 @@ if __name__ == "__main__":
             
                     sent_ids.add(sid)
             
-                    if can_send(sid, 900):
+                    if can_send(sid, 3600):
                         send_telegram(choose_detail_message(sig))
             
             save_state(state)
