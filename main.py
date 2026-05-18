8fbf2f87d4a71c74c332d3722ba54e629fbd332a
Aleks942 <aleks.aw1978@gmail.com>
2026-05-18 18:13:11 +0200

    Update main.py

--- main.py
+++ main.py
@@ -9036,7 +9036,10 @@ def build_signal(instId):
         if (
             premove_score >= 28
             and ep >= 8
-            and has_acceleration
+            and (
+                has_acceleration
+                or has_launch
+            )
             and has_absorption
             and has_launch
         ):
