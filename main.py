68b1fd9c5968fe2e248bf176f5bb31475f9873e3
Aleks942 <aleks.aw1978@gmail.com>
2026-05-28 13:56:32 +0200

    Update main.py

--- main.py
+++ main.py
@@ -8496,7 +8496,6 @@ def decide_entry(stage, flags, price, c5, sig=None):
             if long_strength >= short_strength:
                 print(
                     f"[LONG_DOMINANCE] "
-                    f"{sig.get('instId')} "
                     f"long={long_strength} "
                     f"short={short_strength}",
                     flush=True
@@ -8551,7 +8550,6 @@ def decide_entry(stage, flags, price, c5, sig=None):
             if short_strength >= long_strength:
                 print(
                     f"[SHORT_DOMINANCE] "
-                    f"{sig.get('instId')} "
                     f"short={short_strength} "
                     f"long={long_strength}",
                     flush=True
