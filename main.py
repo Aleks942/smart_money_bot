0a45c768b797b8b22fe7b9b633ecb981a3e778be
Aleks942 <aleks.aw1978@gmail.com>
2026-05-29 15:44:06 +0200

    Update main.py

--- main.py
+++ main.py
@@ -13005,7 +13005,7 @@ def build_signal(instId):
     # =====================
     
     oi = float(
-        sig.get("oi_change") or 0
+        signal.get("oi_change") or 0
     )
 
     volatility_climax_long = (
